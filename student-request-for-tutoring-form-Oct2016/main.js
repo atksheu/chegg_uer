b131565e5aab3435561f40ee9b3e0f6fc98b2abd
atksheu <alan@instaedu.com>
2017-01-14 15:48:09 -0800

    all new prototype

---
+++ student-request-for-tutoring-form-Oct2016/main.js
@@ -0,0 +1,198 @@
+'use strict';
+
+// Shortcuts to DOM Elements.
+var messageForm = document.getElementById('message-form');
+var messageInput = document.getElementById('new-post-message');
+var titleInput = document.getElementById('new-post-title');
+var signInButton = document.getElementById('sign-in-button');
+var signOutButton = document.getElementById('sign-out-button');
+var splashPage = document.getElementById('page-splash');
+var addPost = document.getElementById('add-post');
+var addButton = document.getElementById('add');
+var recentPostsSection = document.getElementById('recent-posts-list');
+var userPostsSection = document.getElementById('user-posts-list');
+var topUserPostsSection = document.getElementById('top-user-posts-list');
+var recentMenuButton = document.getElementById('menu-recent');
+var myPostsMenuButton = document.getElementById('menu-my-posts');
+var myTopPostsMenuButton = document.getElementById('menu-my-top-posts');
+var listeningFirebaseRefs = [];
+
+/**
+ * Saves a new post to the Firebase DB.
+ */
+// [START write_fan_out]
+function writeNewPost(uid, username, picture, title, body) {
+  // A post entry.
+  var postData = {
+    author: username,
+    uid: uid,
+    body: body,
+    title: title,
+    starCount: 0,
+    authorPic: picture
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/posts/' + newPostKey] = postData;
+  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
+// [END write_fan_out]
+
+/**
+ * Starts listening for new posts and populates posts lists.
+ */
+function startDatabaseQueries() {
+  // [START my_top_posts_query]
+  var myUserId = firebase.auth().currentUser.uid;
+  var topUserPostsRef = firebase.database().ref('user-posts/' + myUserId).orderByChild('starCount');
+  // [END my_top_posts_query]
+  // [START recent_posts_query]
+  var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
+  // [END recent_posts_query]
+  var userPostsRef = firebase.database().ref('user-posts/' + myUserId);
+
+  var fetchPosts = function(postsRef, sectionElement) {
+    postsRef.on('child_added', function(data) {
+      var author = data.val().author || 'Anonymous';
+      var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+      containerElement.insertBefore(
+          createPostElement(data.key, data.val().title, data.val().body, author, data.val().uid, data.val().authorPic),
+          containerElement.firstChild);
+    });
+    postsRef.on('child_changed', function(data) { 
+    var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+    var postElement = containerElement.getElementsByClassName('post-' + data.key)[0];
+    postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = data.val().title;
+    postElement.getElementsByClassName('username')[0].innerText = data.val().author;
+    postElement.getElementsByClassName('text')[0].innerText = data.val().body;
+    postElement.getElementsByClassName('star-count')[0].innerText = data.val().starCount;
+    });
+    postsRef.on('child_removed', function(data) {
+    var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+    var post = containerElement.getElementsByClassName('post-' + data.key)[0];
+      post.parentElement.removeChild(post);
+    });
+  };
+
+  // Fetching and displaying all posts of each sections.
+  fetchPosts(topUserPostsRef, topUserPostsSection);
+  fetchPosts(recentPostsRef, recentPostsSection);
+  fetchPosts(userPostsRef, userPostsSection);
+
+  // Keep track of all Firebase refs we are listening to.
+  listeningFirebaseRefs.push(topUserPostsRef);
+  listeningFirebaseRefs.push(recentPostsRef);
+  listeningFirebaseRefs.push(userPostsRef);
+}
+
+/**
+ * Writes the user's data to the database.
+ */
+// [START basic_write]
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+// [END basic_write]
+
+/**
+ * Cleanups the UI and removes all Firebase listeners.
+ */
+function cleanupUi() {
+  // Remove all previously displayed posts.
+  topUserPostsSection.getElementsByClassName('posts-container')[0].innerHTML = '';
+  recentPostsSection.getElementsByClassName('posts-container')[0].innerHTML = '';
+  userPostsSection.getElementsByClassName('posts-container')[0].innerHTML = '';
+
+  // Stop all currently listening Firebase listeners.
+  listeningFirebaseRefs.forEach(function(ref) {
+    ref.off();
+  });
+  listeningFirebaseRefs = [];
+}
+
+/**
+ * The ID of the currently signed-in User. We keep track of this to detect Auth state change events that are just
+ * programmatic token refresh but not a User status change.
+ */
+var currentUID;
+
+/**
+ * Triggers every time there is a change in the Firebase auth state (i.e. user signed-in or user signed out).
+ */
+function onAuthStateChanged(user) {
+  // We ignore token refresh events.
+  if (user && currentUID === user.uid) {
+    return;
+  }
+
+  cleanupUi();
+  if (user) {
+    currentUID = user.uid;
+    splashPage.style.display = 'none';
+    writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+    startDatabaseQueries();
+  } else {
+    // Set currentUID to null.
+    currentUID = null;
+    // Display the splash page where you can sign-in.
+    splashPage.style.display = '';
+  }
+}
+
+/**
+ * Creates a new post for the current user.
+ */
+function newPostForCurrentUser(title, text) {
+  // [START single_value_read]
+  var userId = firebase.auth().currentUser.uid;
+  return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    var username = snapshot.val().username;
+    // [START_EXCLUDE]
+    return writeNewPost(firebase.auth().currentUser.uid, username,
+        firebase.auth().currentUser.photoURL,
+        title, text);
+    // [END_EXCLUDE]
+  });
+  // [END single_value_read]
+}
+
+// Bindings on load.
+window.addEventListener('load', function() {
+  signInButton.addEventListener('click', function() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  });
+
+  // Bind Sign out button.
+  signOutButton.addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+
+  // Listen for auth state changes
+  firebase.auth().onAuthStateChanged(onAuthStateChanged);
+
+  // Saves message on form submit.
+  messageForm.onsubmit = function(e) {
+    e.preventDefault();
+    var text = messageInput.value;
+    var title = titleInput.value;
+    if (text && title) {
+      newPostForCurrentUser(title, text).then(function() {
+        myPostsMenuButton.click();
+      });
+      messageInput.value = '';
+      titleInput.value = '';
+    }
+  };
+
+}, false);
